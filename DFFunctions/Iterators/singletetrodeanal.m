788bfc951aad2b9b766cc4e16d6786cbfbcaa5fe
droumis <roumis.d@gmail.com>
2019-05-02 15:28:39 -0700

    adding tetrode filter evaluators

--- DFFunctions/Iterators/singletetrodeanal.m
+++ DFFunctions/Iterators/singletetrodeanal.m
@@ -1,45 +1,57 @@
-function f = singlecellanal(f, varargin)
-% f = singlecellanal(f)
+function f = singletetrodeanal(f, varargin)
+% f = multicellanal(f)
 % Iterator for a filter object.  Calls the function designated in
 % f().function.name, after loading the variables designated as strings in
 % f().function.loadvariables{:}.  Also the function call appends any
-% options in the f().function.options{} cell array.
+% options in the f().function.options{} cell array.  
+% 
+% Each function call is for one epoch, and it is assumed that the
+% function's first input is a list of indices to the tetrode
+% ([day epoch tetrode]). The second input is a list of exclusion
+% periods [starttime endtime].  The next inputs are the load
+% variables, and the final inputs are the options.  out =
+% fname(index, excludeperiods, var1, var2, ..., option1,
+% option2,...).
 %
-% Each function call is for one cell (or one tetrode), and it is assumed that
-% the function's first input is the index to the cell or tetrode ([day epoch tetrode
-% cell]).  The second input is a list of exclusion periods [starttime endtime].
-% The next inputs are the load variables, and the final inputs are the options.
-% out = fname(index, excludeperiods, var1, var2, ..., option1, option2,...).
-%
-% The output of the call function can either be a 1 by N vector, or a structure.
-% The outputs are stored in f().output, grouped using the same groupings as
-% in the filter.
+% The output of the call function can either be numeric, or a structure.
+% If the output if numeric, data is appended along the first dimenion for
+% the group.
 outputDayEpTetCells = 0; %output into a nested cell structure {day}{ep}{tet}
+allowpairs = 1;
 if ~isempty(varargin)
     assign(varargin{:})
 end
+
 %iterate through all animals
 for an = 1:length(f)
-    %find all unique epochs to analyze for the current animal
-    animaldir = f(an).animal{2};
-    animalprefix = f(an).animal{3};
-    totalepochs = [];
-    for g = 1:length(f(an).epochs)
-        totalepochs = [totalepochs; f(an).epochs{g}];
-    end
-    totaldays = unique(totalepochs(:,1)); %get all of the days across groups
-    
-    %load all the variables that the function requires
-    loadstring = [];
-    for i = 1:length(f(an).function.loadvariables)
-        
-        eval([f(an).function.loadvariables{i}, ' = loaddatastruct(animaldir, animalprefix, f(an).function.loadvariables{i}, totaldays);']);
-        loadstring = [loadstring, f(an).function.loadvariables{i},','];
-    end
-    foptions = f(an).function.options;
+   %find all unique epochs to analyze for the current animal
+   animaldir = f(an).animal{2};
+   animalprefix = f(an).animal{3};
+   totalepochs = [];
+   totaltetrodes = [];
+   for g = 1:length(f(an).epochs)
+      totalepochs = [totalepochs; f(an).epochs{g}];
+      for e = 1:size(f(an).epochs{g},1)
+         totaltetrodes = [totaltetrodes; f(an).data{g}{e}];
+      end
+   end
+   totaldays = unique(totalepochs(:,1)); %get all of the days across groups
+   totaltetrodes = unique(totaltetrodes);
+   
+   %load all the variables that the function requires
+   loadstring = [];
+   for i = 1:length(f(an).function.loadvariables)
+        if (iseegvar(f(an).function.loadvariables{i}))
+         eval([f(an).function.loadvariables{i},' = loadeegstruct(animaldir, animalprefix, f(an).function.loadvariables{i}, totaldays);']);
+        else
+         eval([f(an).function.loadvariables{i},' = loaddatastruct(animaldir, animalprefix, f(an).function.loadvariables{i}, totaldays);']);
+        end
+      loadstring = [loadstring, f(an).function.loadvariables{i},','];
+   end
+   foptions = f(an).function.options;
     
     %iterate through the epochs within each data group
-    fprintf(':::::::: single cell iterator :::::::: \n');
+    fprintf(':::::::: tetrode iterator :::::::: \n');
     for g = 1:length(f(an).epochs)
     for e = 1:size(f(an).epochs{g},1)
     for c = 1:size(f(an).data{g}{e},1)
@@ -48,7 +60,7 @@ for an = 1:length(f)
 
         day = tmpindex(1);
         ep = tmpindex(2);
-        tet = tmpindex(3);
+        tet = tmpindex(3:end);
         foptions = [foptions {'an', an}]; %add the animal index to the varargins
         if outputDayEpTetCells
             eval(['f.output{' num2str(day) '}{' num2str(ep) '}{' num2str(tet) ...
@@ -81,9 +93,6 @@ for an = 1:length(f)
             end
         end
     end
-    end
+   end
     end
 end
-
-
-
--- DFFunctions/createfilter.m
+++ DFFunctions/createfilter.m
@@ -1,6 +1,7 @@
 function f = createfilter(varargin)
 
 f.animal = [];
+f.days= [];
 f.epochs = [];
 f.data = [];
 f.excludetime = [];
@@ -16,7 +17,7 @@ for option = 1:2:length(varargin)-1
         case 'days'
             days = varargin{option+1}; 
         case 'epochs'
-            if exist('days')
+            if exist('days', 'var')
                 f = setfilterepochs(f,varargin{option+1},'days',days);
             else
                 f = setfilterepochs(f,varargin{option+1});
@@ -31,6 +32,10 @@ for option = 1:2:length(varargin)-1
             f = setfiltercells(f,varargin{option+1});
         case 'cellpairs'
             f = setfiltercellpairs(f,varargin{option+1});
+        case 'tetrodes'
+            f = setfiltertetrodes(f, varargin{option+1});
+        case 'tetrodepairs'
+            f = setfiltertetrodepairs(f, varargin{option+1});            
         case 'eegtetrodes'
             f = setfiltereegtetrodes(f, varargin{option+1});
         case 'eegtetrodepairs'
